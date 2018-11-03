6805983def93d822a8a814c68d8b3c8a6cd8ae91
cheneyweb <457299596@qq.com>
2018-11-03 21:29:57 +0800

    首屏加载

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,15 @@ module.exports = {
     name: '我的文明'
   },
 
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      }
+    }
+  },
+
   baseUrl: undefined,
   outputDir: undefined,
   assetsDir: undefined,
