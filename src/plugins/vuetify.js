cb367735b76265d017befeb9125cab3bcf7507e7
cheneyweb <457299596@qq.com>
2019-04-30 12:11:16 +0800

    优化CSS加载

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+// import 'vuetify/dist/vuetify.min.css'
 
 import VueAnimateNumber from 'vue-animate-number'
 Vue.use(VueAnimateNumber)
